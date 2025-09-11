309e7a38d63f297540907f3e96de026797f0be59
Snoopy8403 <gomba.laszlo84@gmail.com>
2025-09-11 14:53:05 +0200

    create of PathOfExile

--- src/Char.java
+++ src/Char.java
@@ -6,4 +6,11 @@ public class Char {
     int strength = 20;
     int dexterity = 20;
     int intelligence = 20;
+
+    //1. isTypeValid method
+    //2. isNameValid min3 max20
+    //3. isLevelValid min1-max100
+    //4. isStrengthValid min0
+    //5. isDexterityValid min0
+    //6. isIntelligenceValid min0
 }
