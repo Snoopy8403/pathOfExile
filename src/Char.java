81c8ec77130c3cc7c3bc2219beceaa4d8901b42e
Snoopy8403 <gomba.laszlo84@gmail.com>
2025-09-12 10:15:43 +0200

    up

--- src/Char.java
+++ src/Char.java
@@ -1,29 +1,81 @@
 
 public class Char {
-    CharType type;
-    String name;
-    int level = 1;
-    int strength = 20;
-    int dexterity = 20;
-    int intelligence = 20;
+    private CharType type;
+    private String name;
+    private int level = 1;
+    private int strength = 20;
+    private int dexterity = 20;
+    private int intelligence = 20;
 
-    boolean isNameValid() {
+    public CharType getType() {
+        return type;
+    }
+
+    public void setType(CharType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public boolean isTypeValid(){
+       return type != null;
+   }
+
+    public boolean isNameValid() {
         return name.length() >= 3 && name.length() <= 20;
     }
 
-    boolean isLevelValid() {
+    public boolean isLevelValid() {
         return level >= 1 && level <= 100;
     }
 
-    boolean isStrengthValid() {
+    public boolean isStrengthValid() {
         return strength >= 0;
     }
 
-    boolean isDexterityValid() {
+    public boolean isDexterityValid() {
         return dexterity >= 0;
     }
 
-    boolean isIntelligenceValid() {
+    public boolean isIntelligenceValid() {
         return intelligence >= 0;
     }
 
